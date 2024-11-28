92504c2a2f282d3e778ac49567c38d8f6ff06f94
NguyenMinhDuccc <ducnmph46687@fpt.edu.vn>
2024-11-28 18:53:15 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import MyComponents from './components/MyComponents';
+  
+const App = () => {
+  return (
+    <>
+    <MyComponents></MyComponents>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/MyComponents.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const MyComponents = () => {
+  handleOnChange = () =>{
+    
+  }
+  return (
+    <div>MyComponents is </div>
+  )
+}
+export default MyComponents
